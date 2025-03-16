73c2db1ffe3adba4bdfd1ac15881bc4655c95016
kokoro-hart <tobita@wd-flat.com>
2025-03-17 08:55:41 +0900

    feat: initial commit

---
+++ src/hooks/useTasks.js
@@ -0,0 +1,49 @@
+import { useLocalStorageState } from "./useLocalStorageState";
+
+export function useTasks() {
+  const [taskList, setTaskList] = useLocalStorageState("taskList", []);
+
+  const activeTaskList = taskList.filter(({ status }) => status !== "trashed");
+
+  const trashedTaskList = taskList.filter(({ status }) => status === "trashed");
+
+  const createTask = (title) => {
+    setTaskList((prevTaskList) => {
+      const newTask = {
+        id: Date.now(),
+        title,
+        status: "notStarted",
+      };
+      return [...prevTaskList, newTask];
+    });
+  };
+
+  const updateTask = (id, updatedTask) => {
+    setTaskList((prevTaskList) => {
+      return prevTaskList.map((task) =>
+        task.id === id ? { ...task, ...updatedTask } : task,
+      );
+    });
+  };
+
+  const deleteTask = (id) => {
+    setTaskList((prevTaskList) => {
+      return prevTaskList.filter((task) => task.id !== id);
+    });
+  };
+
+  const deleteAllTrashedTasks = () => {
+    setTaskList((prevTaskList) => {
+      return prevTaskList.filter((task) => task.status !== "trashed");
+    });
+  };
+
+  return {
+    activeTaskList,
+    trashedTaskList,
+    createTask,
+    updateTask,
+    deleteTask,
+    deleteAllTrashedTasks,
+  };
+}
